57fc58d4043986f2478274b9e51c7b26f35c75bf
shanemcdaid96 <L00117529@student.lyit.ie>
2018-11-09 18:37:04 +0000

    Added calc.java

---
+++ calc.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class calc
+{
+          public static void main(String[] args)
+          {
+               int num1, num2;
+
+              Scanner input = new Scanner(System.in);
+              System.out.println("Enter Num 1: ");
+              num1=input.nextInt();
+              System.out.println("Enter Num 2: ");
+              num2=input.nextInt();
+
+              System.out.println("Answer: " + (num1+num2));
+           }
+} 
